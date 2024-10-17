087757152ff92e9e97f9baf3200b2a999eb8d86e
JAGRIT KUMAR <e9651@E9651-TECH-2.local>
2024-10-17 22:47:18 +0530

    Onboarding page with openweathermap API

---
+++ src/components/CityInput/CityInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useWeather } from "../../context/WeatherContext";
+import Button from "../Button/Button";
+import styles from "./CityInput.module.css";
+
+const CityInput = () => {
+  const [inputCity, setInputCity] = useState("");
+  const { setCity, fetchWeather } = useWeather();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputCity.trim() !== "") {
+      setCity(inputCity); // Update city in context
+      fetchWeather(inputCity); // Fetch weather data
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={inputCity}
+          onChange={(e) => setInputCity(e.target.value)}
+          className={styles.input}
+        />
+        <Button></Button>
+      </form>
+    </div>
+  );
+};
+
+export default CityInput;
---
+++ src/components/presenationLayer/Onboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CityInput from "../CityInput/CityInput";
+
+function Onboard() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <CityInput />
+
+    </div>
+  );
+}
+
+export default Onboard;
