f6b917594b54dfd794ffda7dc840839e72eaf5a4
Jagrit65 <115291205+Jagrit65@users.noreply.github.com>
2024-10-18 13:22:15 +0530

    Update App.js
    on board import fixess

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { WeatherProvider } from './context/WeatherContext';
 import './App.css';
-import Onboard from './components/PresenationLayer/Onboard';
+import Onboard from './components/presenationLayer/Onboard';
 
 function App() {
   return (
